e87d6bed607ca205761d714d6d2f28108e546fe6
sabotack <sabotack.2001@gmail.com>
2022-01-02 14:01:57 +0000

    Initial source code commit

---
+++ src/bot/myBot.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include "discord.h"
+
+#define NUM_OF_COMMENTS 18
+
+void on_ready(struct discord *client);
+void on_viktor(struct discord *client, const struct discord_message *msg);
+
+int main(void) {
+
+    struct discord *client = discord_config_init("./mybot_config.json");
+
+    discord_set_on_ready(client, &on_ready);
+
+    discord_set_on_message_create(client, &on_viktor);
+
+    discord_run(client);
+
+    discord_cleanup(client);
+
+    return 0;
+}
+
+
+void on_ready(struct discord *client) {
+    const struct discord_user *bot = discord_get_self(client);
+
+    log_info("Succesfully connected to Discord as %s#%s!",
+        bot->username, bot->discriminator);
+}
+
+void on_viktor(struct discord *client, const struct discord_message *msg) {
+    if(msg->author->bot) return;
+
+    char *message = strdup(msg->content);
+
+    for(int i = 0; i < strlen(message); i++) {
+        message[i] = tolower(message[i]);
+    }
+
+    if(strstr(message, "viktor") != NULL) {
+        int randNum = 0;
+
+        char *hateComments[50] = {
+            "all my homies hate viktor...",
+            "i hate viktor",
+            "shut up viktor",
+            "hey victor",
+            "vIkToR",
+            "imagine being viktor xddd",
+            "viktor is cringe",
+            "cringe viktor",
+            "stfu viktor",
+            "viktor did anyone ask?",
+            "ur gae viktor",
+            "viktor? xdddd",
+            "viktor is so gae",
+            "i just hate viktor so much",
+	    "get ratioed viktor",
+	    "viktor fell off",
+	    "the hood hates viktor now",
+	    "viktor get ratioed + ur white + u fell off"
+        };
+
+        srand(time(NULL));
+        randNum = rand() % NUM_OF_COMMENTS;
+
+        struct discord_create_message_params params = { .content = hateComments[randNum] };
+        discord_create_message(client, msg->channel_id, &params, NULL);
+    }
+}
+
