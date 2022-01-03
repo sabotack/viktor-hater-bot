884ddd76d5f371de1e525f74e6d8932187289a24
sabotack <sabotack.2001@gmail.com>
2022-01-03 22:07:57 +0100

    Segmentation fault fix attempt #1
    - now using strcpy instead of strdup, when copying the message text to a temporary string

--- src/bot/myBot.c
+++ src/bot/myBot.c
@@ -17,7 +17,7 @@ int newRoast = 0;
 int main(void) {
 
     struct discord *client = discord_config_init("./../../../mybot_config.json");
-    
+    disc
     discord_set_on_ready(client, &on_ready);
     discord_set_on_message_create(client, &on_viktor);
 
@@ -36,8 +36,10 @@ void on_ready(struct discord *client) {
 
 void on_viktor(struct discord *client, const struct discord_message *msg) {
     if(msg->author->bot) return;
-    
-    char *message = strdup(msg->content);
+    int msgLength = sizeof(msg)/sizeof(char);
+
+    char message[msgLength];
+    strcpy(msg, message);
 
     for(int i = 0; i < strlen(message); i++) {
         message[i] = tolower(message[i]);
@@ -107,6 +109,6 @@ void on_viktor(struct discord *client, const struct discord_message *msg) {
         }
     }
 
-    free(message);
+    /* free(message); */
 }
 
